2125408d72cf533512050f70633175e2eedd203d
rodrigobarr0s <rodrigobarros.dev@gmail.com>
2025-03-02 10:39:13 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,24 +1,16 @@
 package application;
 
-import java.sql.Date;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-
-		Department obj = new Department(1, "Books");
-
-		Seller seller = new Seller(21, "Bob", "bob@gmail.com",
-				Date.valueOf(LocalDate.parse("22/05/1985", DateTimeFormatter.ofPattern("dd/MM/yyyy"))), 3000.0, obj);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
 	}
--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -12,19 +12,19 @@ public class Seller implements Serializable {
 	private String name;
 	private String email;
 	private Date birthDate;
-	private Double salary;
+	private Double baseSalary;
 
 	private Department department;
 
 	public Seller() {
 	}
 
-	public Seller(Integer id, String name, String email, Date birthDate, Double salary, Department department) {
+	public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) {
 		this.id = id;
 		this.name = name;
 		this.email = email;
 		this.birthDate = birthDate;
-		this.salary = salary;
+		this.baseSalary = baseSalary;
 		this.department = department;
 	}
 
@@ -60,12 +60,12 @@ public class Seller implements Serializable {
 		this.birthDate = birthDate;
 	}
 
-	public Double getSalary() {
-		return salary;
+	public Double getBaseSalary() {
+		return baseSalary;
 	}
 
-	public void setSalary(Double salary) {
-		this.salary = salary;
+	public void setBaseSalary(Double baseSalary) {
+		this.baseSalary = baseSalary;
 	}
 
 	public Department getDepartment() {
@@ -96,7 +96,7 @@ public class Seller implements Serializable {
 	@Override
 	public String toString() {
 		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", birthDate=" + birthDate + ", salary="
-				+ salary + ", department=" + department + "]";
+				+ baseSalary + ", department=" + department + "]";
 	}
 
 }
